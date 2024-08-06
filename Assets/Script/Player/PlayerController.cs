387c39687f96d86004c24c6be65a55532776a888
zood2k12 <hnqmondadori@gmail.com>
2024-08-06 13:29:30 -0400

    Lab 3 (Late submission August 6th) - Fixed
    Aug 6th
    // Fixed the Attack and JumpAttack animation.
    // Was having issues with the Animator window and parameters. Had to do few changes on the code where I typed wrong few letters.
    
    Current Error to be fixed:
    'Player' AnimationEvent has no function name specified!

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -2,17 +2,111 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animation))]
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
+    //Player gameplay variables
+    private Coroutine jumpForceChange;
+    private Coroutine speedChange;
+
+    //Remove the comment below when ready; used to finish the Lab 4
+    /*public void PowerupValueChange(Pickup.PickupType type)
+    {
+        if (type == Pickup.PickupType.PowerupSpeed)
+            StartPowerupCoroutine(ref speedChange, ref speed, type);
+
+        if (type == Pickup.PickupType.PowerupJump)
+            StartPowerupCoroutine(ref jumpForceChange, ref jumpForce, type);
+    }
+
+    public void StartPowerupCoroutine(ref Coroutine InCoroutine, ref float inVar, Pickup.PickupType type)
+    {
+        if (InCoroutine != null)
+        {
+            StopCoroutine(InCoroutine);
+            InCoroutine = null;
+            inVar /= 2;
+        }
+
+        InCoroutine = StartCoroutine(PowerupChange(type));
+    }
+
+    IEnumerator PowerupChange(Pickup.PickupType type)
+    {
+        //this code runs before the wait
+        if (type == Pickup.PickupType.PowerupSpeed)
+            speed *= 2;
+
+        if (type == Pickup.PickupType.PowerupJump)
+            jumpForce *= 2;
+
+        Debug.Log($"Jump force value is {jumpForce}, Speed value is {speed}");
+
+        yield return new WaitForSeconds(5.0f);
+
+        if (type == Pickup.PickupType.PowerupSpeed)
+        {
+            speed /= 2;
+            speedChange = null;
+        }
+        if (type == Pickup.PickupType.PowerupJump)
+        {
+            jumpForce /= 2;
+            jumpForceChange = null;
+        }
+
+        Debug.Log($"Jump force value is {jumpForce}, Speed value is {speed}");
+    }*/
+
+    //Private Lives Variable
+    private int _lives = 10;
+
+    //public variable for getting and setting lives
+    public int lives
+    {
+        get
+        {
+            return _lives;
+        }
+        set
+        {
+            //all lives lost (zero counts as a life due to the check)
+            if (value < 0)
+            {
+                //game over function called here
+                //return to prevent the rest of the function to be called
+                return;
+            }
+
+            //lost a life
+            if (value < _lives)
+            {
+                //Respawn function called here
+            }
+
+            //cannot roll over max lives
+            if (value > maxLives)
+            {
+                value = maxLives;
+            }
+
+            _lives = value;
+
+            Debug.Log($"Lives value on {gameObject.name} has changed to {lives}");
+        }
+    }
+
+    //max lives that are possible
+    [SerializeField] private int maxLives = 10;
+
     //Movement Variables
     [SerializeField, Range(1, 20)] private float speed = 5;
     [SerializeField, Range(1, 20)] private float jumpForce = 10;
     [SerializeField, Range(0.01f, 1)] private float groundCheckRadius = 0.02f;
     [SerializeField] private LayerMask isGroundLayer;
 
-    //GroundCheck stuff
-    private Transform groundcheck;
+    //GroundCheck Stuff
+    private Transform groundCheck;
     private bool isGrounded = false;
 
     //Component References
@@ -20,17 +114,15 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer sr;
     Animator anim;
 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        //Components References Filled *Need to add these components everytime you add comp referecens*
+        //Component References Filled
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
-        Debug.Log(rb.name);
 
+        //Checking values to ensure non garbage data
         if (speed <= 0)
         {
             speed = 5;
@@ -44,38 +136,60 @@ public class PlayerController : MonoBehaviour
         }
 
         //Creating groundcheck object
-        if (!groundcheck)
+        if (!groundCheck)
         {
             GameObject obj = new GameObject();
             obj.transform.SetParent(transform);
             obj.transform.localPosition = Vector3.zero;
             obj.name = "GroundCheck";
-            groundcheck = obj.transform;
+            groundCheck = obj.transform;
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Create a small overlap collider to check if we are touching the ground
-        IsGrounded();
-        
+        AnimatorClipInfo[] curPlayingClips = anim.GetCurrentAnimatorClipInfo(0);
         //grab horizontal axis - Check Project Settings > Input Manager to see the inputs defined
         float hInput = Input.GetAxis("Horizontal");
 
+        //Create a small overlap collider to check if we are touching the ground
+        IsGrounded();
 
 
-        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        //Animation check for our physics
+        if (curPlayingClips.Length > 0)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-
+            if (curPlayingClips[0].clip.name == "Attack")
+            {
+                if (isGrounded)
+                    rb.velocity = Vector2.zero;
+                //new Vector2(0, rb.velocity.y);
+            }
+            else
+                rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
         }
 
 
-        //Sprite Flipling
+        //Button Input Checks
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!isGrounded && curPlayingClips[0].clip.name != "JumpAttack")
+            {
+                anim.SetTrigger("JumpAttack");
+            }
+            else if (!curPlayingClips[0].clip.name.Contains("Attack"))
+            {
+                anim.SetTrigger("Fire");
+            }
+        }
+
+        //Sprite Flipping
         if (hInput != 0) sr.flipX = (hInput < 0);
         //if (hInput > 0 && sr.flipX || hInput < 0 && !sr.flipX) sr.flipX = !sr.flipX;
 
@@ -83,16 +197,25 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("isGrounded", isGrounded);
 
     }
+
+    /// <summary>
+    /// This function is used to check if we are grounded.  When we jump - we disable checking if we are grounded until our velocity reaches negative on the y-axis - this indicates that we are falling and we should start to check if we are grounded again. This is done to prevent us flipping to grounded when we jump through a platform.
+    /// </summary>
     void IsGrounded()
     {
         if (!isGrounded)
         {
             if (rb.velocity.y <= 0)
             {
-                isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, isGroundLayer);
+                isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
             }
         }
         else
-            isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, isGroundLayer);
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+    }
+
+    void IncreaseGravity()
+    {
+        rb.gravityScale = 10;
     }
 }
