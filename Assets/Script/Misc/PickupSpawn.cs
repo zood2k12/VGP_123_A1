eaf955f367b44fb266f401bd55b74e46a5217f99
zood2k12 <hnqmondadori@gmail.com>
2024-08-26 13:44:18 -0400

    Lab 5 - Completed
    Aug 26th
    // All Labs until Lab 5 completed and fixed

--- Assets/Script/Misc/PickupSpawn.cs
+++ Assets/Script/Misc/PickupSpawn.cs
@@ -1,42 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 
 public class PickupSpawn : MonoBehaviour
 {
-    //Script it`s a bit less generic and have to check for more random ones to apply for future projects
-    //This script serves to spawn randomly for Lab 5, solution
-
-    [SerializeField, Range(0, 2)] private float Element;
-
-    [HideInInspector]
-    public float randXPos;
-    [HideInInspector]
-    public float randYPos;
-
-    public GameObject spawnPrefab;
-
-    
-    
+    public GameObject[] spawnPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-       
-        float randXPos = Random.Range(-8.05f, 200.01f);
-        float randYPos = Random.Range(-8.05f, 200.01f);
-        Vector2.position = transform.position;
+        float randXPos = Random.Range(1.5f, 20f);
+        float randYPos = Random.Range(1.2f, 9f);
 
-        transform.position = Vector2(randXPos, randYPos);
+        transform.position = new Vector2(randXPos, randYPos);
 
         int randNum = Random.Range(0, spawnPrefab.Length);
 
         if (spawnPrefab[randNum] == null) return;
 
-        Instantiate(spawnPrefab[randNum], transform.position, transform.rotation
-        
+        Instantiate(spawnPrefab[randNum], transform.position, transform.rotation);
+       
     }
-    
- 
 }
